7ae6b1a642b855c1dc707a629b28fac25063bf26
hameedulhaq <hameed.ulhaq@auto.mynaghi.com>
2025-11-24 12:39:57 +0300

    first commit

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Hello World! This is my first application"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
